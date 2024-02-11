76aa7143fcc5c8aedbb65e4adb0d5c32c427acbf
Hadong <dfgh119@naver.com>
2024-02-11 23:22:55 +0900

    1

--- whisper.py
+++ whisper.py
@@ -18,8 +18,7 @@ root.withdraw()
 file_path = filedialog.askopenfilename()
 file_name = file_path.split("/")[-1]
 
-file_name = '240207_1359_10449'
-result = model.transcribe("/content/drive/MyDrive/voice_record/file_name")
+result = model.transcribe("/Users/poca/Library/CloudStorage/OneDrive-공유라이브러리-onedrive/녹음본/FOLDER01/" + file_name)
 print(result["text"])
 
 def save_text_to_file(text, filepath):
@@ -30,7 +29,7 @@ def save_text_to_file(text, filepath):
 text_value = result["text"]
 
 # 텍스트를 저장할 파일 경로를 지정합니다. 필요에 따라 경로와 파일 이름을 수정하세요.
-filepath = f"/content/drive/MyDrive/voice_record/txt/{file_name[:-4]}.txt"
+filepath = f"/Users/poca/Library/CloudStorage/OneDrive-공유라이브러리-onedrive/녹음본/txt/{file_name[:-4]}.txt"
 
 # 텍스트를 파일로 저장합니다.
 save_text_to_file(text_value, filepath)
